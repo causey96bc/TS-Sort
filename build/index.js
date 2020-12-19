2fbed2c822713f3977a830681d7107d9a88b1007
causey904.bc <your_email@real_email.edu>
2020-12-19 12:18:04 -0500

    initial commit

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hi there");
+var logsomething = function () {
+    console.log("hell no");
+};
+logsomething();
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hi there");
+const logsomething = () => {
+   console.log("hell no");
+ };
+  logsomething();
